d10fee5b04fc60289a1453bba49e062120f4cb5f
dumbu1 <krishnamohan.balivada@gmail.com>
2021-12-06 21:11:32 +0530

    commit 2

--- src/main/java/util/PropertyReader.java
+++ src/main/java/util/PropertyReader.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyReader {
+	
 private static 	FileInputStream obj_FileInputStream;
 	
 public  Properties    propertyReader() {
 	try {
-		 obj_FileInputStream=new FileInputStream("");
+		 obj_FileInputStream=new FileInputStream(System.getProperty("user.dir")+ "/src/test/resources/configuration/config.properties");
 	} catch (FileNotFoundException e) {
 		 System.out.println(e);
 	}
